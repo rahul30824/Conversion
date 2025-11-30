294321d5401e00dd7b65969b16df8ab7783bb7e6
Rahul_Ramamoorthy:) <rahulr684.sse@saveetha.com>
2025-11-30 14:28:55 +0530

    Update Conversion.java

--- Conversion.java
+++ Conversion.java
@@ -4,7 +4,7 @@ class Main {
     public static void main(String[] args) {
 
         // 1. char[] → String
-        char[] array1 = {'a','b','c','d'};
+        char[] array1 = {'a','b','c','d'};//direct conversion//
         String newstr = new String(array1);
         System.out.println(newstr);
 
@@ -15,20 +15,8 @@ class Main {
             newarr[i] = num.charAt(i) - '0';
         }
         System.out.println(Arrays.toString(newarr));
-
-        // 3. String → char[]
-        char[] newchar = newstr.toCharArray();
-        System.out.println(Arrays.toString(newchar));
-
-        // 4. String → char
-        char ch = newstr.charAt(0);
-        System.out.println(ch);
-
-        // 5. char → String
-        String cts = String.valueOf(ch);
-        System.out.println(cts);
-
-        // 6. int[] → String (manual concat)
+        
+         // 3. int[] → String (manual concat)
         int[] array2 = {1,2,3,4,5};
         String result = "";
         for (int n : array2) {
@@ -36,11 +24,37 @@ class Main {
         }
         System.out.println(result);
 
-        // 7. int[] → String (official)
+        int num2=456;
+        String newStr=String.valueOf(num2);
+              //(or)//
+        //String newstr = Integer.toString(num2);
+        System.out.println(newStr);
+        
+        // 4. String → char[]
+        char[] newchar = newstr.toCharArray();//direct conversion//
+        System.out.println(Arrays.toString(newchar));
+
+        // 5. String → char
+        char ch = newstr.charAt(0);
+        System.out.println(ch);
+
+        // 6. char → String
+        String cts = String.valueOf(ch);
+        System.out.println(cts);
+
+       // 7. int[] → String (official)
         String official = Arrays.toString(array2);
         System.out.println(official);
-
-        // 8. String → String[] (split)
+        
+         //8.int[]--> String(without commas)//
+        int[] num3={1,2,3,4,5};
+        String result1="";
+        for(int n:num3)
+        {
+            result1=result1+n;
+        }
+        System.out.println(result1);
+         // 9. String → String[] (split)
         String sentence = "hi hello welcome";
         String[] parts = sentence.split(" ");
         System.out.println(Arrays.toString(parts));
